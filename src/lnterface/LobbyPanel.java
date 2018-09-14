883917c3806918e301495dbb10f3ffaf503cb960
juandavidpaz <juandavidpaz@172.30.180.147>
2018-09-14 16:03:52 -0500

    LobbyPanel modified

--- src/lnterface/LobbyPanel.java
+++ src/lnterface/LobbyPanel.java
@@ -8,8 +8,8 @@ import java.awt.event.ActionListener;
 public class LobbyPanel extends JPanel implements ActionListener {
 	
 
-	public static final Image LEVEL = Toolkit.getDefaultToolkit()
-			.createImage("./images/logos/level.png");
+	public static final Image FOLDER = Toolkit.getDefaultToolkit()
+			.createImage("./images/logos/folder.png");
 	
 	public static final Image SEASON = Toolkit.getDefaultToolkit()
 			.createImage("./images/logos/season1.png");
@@ -27,9 +27,9 @@ public class LobbyPanel extends JPanel implements ActionListener {
 	private MatchmakingPanel matchmaking;
 	private JButton go;
 	private JButton play;
-	private JLabel logo;
+	private JLabel name;
 	private JLabel level;
-	private JTextField txtLevel; 
+	private JLabel iLevel; 
 	
 
 	public LobbyPanel(InitialPanel initial) {
@@ -40,23 +40,58 @@ public class LobbyPanel extends JPanel implements ActionListener {
 		this.initial = initial;
 		
 		
+		
 		matchmaking = new MatchmakingPanel(this);
+		
+		play = new JButton("PLAY");
+		play.addActionListener(this);
+		play.setActionCommand(PLAY);
+		//play.setBackground(new Color(243,229,60));
+		play.setBackground(Color.BLUE);
 
-		go = new JButton("Go");
+		
+		
+		
+		
+		name = new JLabel("JUANCHOTAGRANDE", SwingConstants.CENTER);
+		name.setLayout(null);
+		name.setFont(new Font("Broadway", 1, 24));
+		name.setForeground(Color.WHITE);
+		
+		level = new JLabel("LEVEL");
+		level.setLayout(null);
+		level.setFont(new Font("Broadway", 1, 36));
+		level.setForeground((Color.WHITE));
+		
+		iLevel = new JLabel("40");
+	    iLevel.setLayout(null);
+		iLevel.setFont(new Font("Broadway", 1, 36));
+		iLevel.setForeground((Color.WHITE));
 
-		go.setBounds(50, 400, 50, 50);
-		go.setActionCommand("go");
-		go.addActionListener(this);
+		
 
-		add(go);
+		
+
+
+		
+		Components();
 
 	}
 
 	
 	public void Components ()
 	{
+		level.setBounds(95, 200, 200, 200);
+		iLevel.setBounds(125,250,200,200);
+		name.setBounds(5,280,300,250);
+		play.setBounds(1000, 600, 150, 80);
+
 		
 		
+		add(level);
+		add(iLevel);
+		add(name);
+		add(play);
 		
 		
 	}	
@@ -73,7 +108,8 @@ public class LobbyPanel extends JPanel implements ActionListener {
 		g.drawImage(SEASON, 10, 80, null);
 		g.drawImage(OPTIONS, 190, 2, null);
 		g.drawImage(LOGO, 90, 140, null);
-		g.drawImage(LOGO, 90, 140, null);
+		g.drawImage(FOLDER, 5, 450, null);
+
 
 
 
